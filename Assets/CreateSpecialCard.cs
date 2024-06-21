90a996682a6772335d67fe4b2890de401d5214e6
sungilan <asiverver@naver.com>
2024-06-21 17:38:09 +0900

    ㅋㅋ

--- Assets/CreateSpecialCard.cs
+++ Assets/CreateSpecialCard.cs
@@ -21,7 +21,7 @@ public class CreateSpecialCard : MonoBehaviour
     /// Start is called before the first frame update
     void Start()
     {
-        SetSpecialCard();
+        //SetSpecialCard();
     }
 
     /// Update is called once per frame
